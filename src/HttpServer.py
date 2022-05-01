5539ab73d1a0963d6737032e643630b71f9fe79d
pihitpihit <pihitpihit@gmail.com>
2022-05-01 10:34:07 +0000

    bot <-> http server 통신 성공

--- src/HttpServer.py
+++ src/HttpServer.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
 
-class HttpServer(BaseHTTPRequestHandler):
+class HttpServerHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-Type', 'text/html; charset=utf-8')
@@ -10,9 +11,49 @@ class HttpServer(BaseHTTPRequestHandler):
         self.wfile.write('<h1>안녕하세요</h1>'.encode('utf-8'))
         return
 
+    def do_POST(self):
+        contentLength = self.contentLength()
+        data = self.data()
+
+        print('[do_POST] path          = %s' % self.path)
+        print('[do_POST] contentLength = %d' % contentLength)
+        print('[do_POST] data          = %s' % data)
+
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json; charset=utf-8')
+        self.end_headers()
+
+        dataRes = (json.dumps(self.helloWorldResponse()) + '\n').encode('utf-8')
+        self.wfile.write(dataRes)
+        return
+
+    def contentLength(self):
+        return int(self.headers.get('Content-Length'))
+
+    def data(self):
+        return json.loads(self.rfile.read(self.contentLength()))
+
+    def helloWorldResponse(self):
+        dictRes = {}
+        dictRes['version'] = '1.0'
+        dictRes['data'] = {
+            'msg': 'Hello Eightanium'
+        }
+        return dictRes
+
+class HttpServer:
+    def __init__(self):
+        return
+
+    def run(self, port):
+        httpd = HTTPServer(('0.0.0.0', 8888), HttpServerHandler)
+        httpd.serve_forever()
+        return
+
+
 def main():
-    httpd = HTTPServer(('0.0.0.0', 8888), HttpServer)
-    httpd.serve_forever()
+    hs = HttpServer()
+    hs.run(port=8888)
     return
 
 if __name__ == '__main__':
