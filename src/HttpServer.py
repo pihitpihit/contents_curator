0f26b0cce47f710dbae822a5a4859504febfa15b
pihitpihit <pihitpihit@gmail.com>
2022-05-01 06:36:10 +0000

    http서버 동작하도록 수정

--- src/HttpServer.py
+++ src/HttpServer.py
@@ -3,10 +3,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class HttpServer(BaseHTTPRequestHandler):
-    def __init__(self, port):
-        self.port = port
-        return
-
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-Type', 'text/html; charset=utf-8')
@@ -14,13 +10,9 @@ class HttpServer(BaseHTTPRequestHandler):
         self.wfile.write('<h1>안녕하세요</h1>'.encode('utf-8'))
         return
 
-    def run(self):
-        httpd = HTTPServer('0.0.0.0', self.port), HttpServer)
-        httpd.serve_forever()
-        return
-
 def main():
-    hs = HttpServer(9999)
+    httpd = HTTPServer(('0.0.0.0', 8888), HttpServer)
+    httpd.serve_forever()
     return
 
 if __name__ == '__main__':
